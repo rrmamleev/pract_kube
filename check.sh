3cd712f69e658c6e4322522db8a139c0f76db87e
s056568 <lvovas@gmail.com>
2023-05-14 19:36:31 +0000

    Update file check.sh

--- check.sh
+++ check.sh
@@ -6,10 +6,10 @@ for service in $SERVICES
 do
   echo "Checking status of $service..."
   STATUS=$(docker-compose ps -q $service | xargs docker inspect -f '{{ .State.Health.Status }}')
-  if [ "$STATUS" != "running" ]; then
-    echo "Service $service is not running"
+  if [ "$STATUS" != "healthy" ]; then
+    echo "Service $service is not healthy"
     exit 1
   fi
 done
 
-echo "All services are running"
+echo "All services are healthy"
