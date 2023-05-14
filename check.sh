1426eed432ade70d82d03e0e54fd6da3ae5cdc20
s056568 <lvovas@gmail.com>
2023-05-14 17:30:23 +0000

    Update 2 files
    - /check.sh
    - /.gitlab-ci.yml

---
+++ check.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+SERVICES="redis flask_app"
+
+for service in $SERVICES
+do
+  echo "Checking status of $service..."
+  STATUS=$(docker-compose ps -q $service | xargs docker inspect -f '{{ .State.Status }}')
+  if [ "$STATUS" != "running" ]; then
+    echo "Service $service is not running"
+    exit 1
+  fi
+done
+
+echo "All services are running"
